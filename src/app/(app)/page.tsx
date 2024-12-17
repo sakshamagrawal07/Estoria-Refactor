224e451bd47a1616aca4fa9ee96d2adaf1e7de1a
Sambodhi <sambodhibhowal@Gmail.com>
2024-12-18 00:22:51 +0530

    Updated Past event data

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,6 +4,8 @@ import AnimatedTitle from "@/components/home/animatedTitle";
 import "./globals.css"
 import Card from "@/components/home/card";
 
+
+
 export default function Home() {
   return (
     <>
